044068e5cfd7a35fe3844a9e562a13c374c4720f
M-hanif <m.hanifformoly@gmail.com>
2019-09-14 17:55:24 +0430

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -1,23 +1,15 @@
 document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+    document.addEventListener("backbutton", function (e) {
+        e.preventDefault(); 
+    navigator.notification.confirm("Are you sure want to exit from App?", onConfirmExit, "Confirmation", "Yes,No");
+    }, false );
+}
 
-
-    function onDeviceReady()
-    {
-        // Add event listener to the back button
-        document.addEventListener("backbutton", backButtonHandler, false);
-    }
-    
-    function backButtonHandler(e )
-   {
-	   alert("hellooooo");
-if( $("#homepageLanding").length > 0 )
-      {
-         // Code to exit app
-         navigator.app.exitApp();
-      }
-      else
-      {
-        // Code to navigate to the history action
-         navigator.app.backHistory();
-      }
+function onConfirmExit(button) {
+	if(button==2){ //If User select a No, then return back;
+		return;
+	}else{
+		navigator.app.exitApp(); // If user select a Yes, quit from the app.
+	}
 }
